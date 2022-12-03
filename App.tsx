247507f6d46fbee4723c98178b2b0f401d88f98b
kaique araujo lopes <kaiquearaujo246@hotmail.com>
2022-12-03 07:16:45 -0300

    starting navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import theme from "./src/theme";
 
 import { Groups } from "./src/screens/Groups";
+import { NewGroup } from "./src/screens/NewGroup";
+import { Players } from "./src/screens/Players";
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -12,9 +14,13 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {
-        fontsLoader ? <Groups /> : <Loading />
-      }
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent"
+        translucent
+      />
+      
+      { fontsLoader ? <Players /> : <Loading /> }
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,44 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+
+import { Text, View, FlatList } from 'react-native';
+
+import { Header } from '../../components/Header';
+import { Highlight } from '../../components/Highlight';
+import { GroupCard } from '../../components/GroupCard';
+import { ListEmpty } from '../../components/ListEmpty';
+import { Button } from '../../components/Button';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
-   
-        
-       
+      <Header />
+
+      <Highlight
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
+      
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primieiro turma?" />
+        )}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
+
+      <Button 
+        title="Criar nova turma" 
+      />
     </Container>
   );
 }
